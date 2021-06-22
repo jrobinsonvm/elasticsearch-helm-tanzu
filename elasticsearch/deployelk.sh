c1cb3809b69afd61ffb131c46b993c4c05d83988
jrobinsonvm <73367284+jrobinsonvm@users.noreply.github.com>
2021-06-22 12:35:23 -0400

    Update deployelk.sh

--- elasticsearch/deployelk.sh
+++ elasticsearch/deployelk.sh
@@ -5,3 +5,4 @@ helm install elk-master .  -n elk  -f master-values.yaml
 helm install elk-data .  -n elk  -f data-node-values.yaml
 helm install elk-ml .  -n elk  -f machine-learning-values.yaml
 helm install elk-lb .  -n elk  -f coordinating-only-values.yaml
+helm ls -n elk
